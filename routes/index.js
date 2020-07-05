a1ef2f472ba0dc2aa6d69064731be623c95d1c95
JaneGirshin <Jane@girsh.in>
2020-07-05 15:25:19 +0300

    Landing page styling

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../models/book');
+const Author = require('../models/author');
 
 router.get('/', async (req, res) => {
-    let books;
+    let books, authors;
     try {
         books = await Book.find().sort({ createdAt: 'desc' }).limit(10).exec();
+        authors = await Author.find().sort({ name: 'desc' }).limit(10).exec();
     } catch {
         books = []
+        authors = []
     }
-    res.render('index', { books: books });
+    res.render('index', { books: books, authors: authors });
 })
 
 module.exports = router;
